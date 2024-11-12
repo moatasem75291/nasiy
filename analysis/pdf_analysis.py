66e7066e69b77caca1d44a7fab93632f509930c0
moatasem mohammed <47195525+moatasem75291@users.noreply.github.com>
2024-11-12 14:41:39 +0200

    Update pdf_analysis.py

--- analysis/pdf_analysis.py
+++ analysis/pdf_analysis.py
@@ -23,7 +23,7 @@ def generate_wordcloud(text):
     bidi_text = get_display(reversed_text)
 
     wordcloud = WordCloud(
-        font_path="arial.ttf",
+        font_path=font_path,
         background_color="white",
         width=800,
         height=400,
