78b794e5b434ec41a577491cb479e31b7d7d13d5
Mo3tsm <mo5les75291@gmail.com>
2024-11-12 15:16:30 +0200

    Update pdf_analysis.py to use default font and modify requirements.txt for pillow dependency

--- analysis/pdf_analysis.py
+++ analysis/pdf_analysis.py
@@ -22,8 +22,11 @@ def generate_wordcloud(text):
     reversed_text = reverse_words(reshaped_text)
     bidi_text = get_display(reversed_text)
 
+    from PIL import ImageFont
+
+    font = ImageFont.load_default()
     wordcloud = WordCloud(
-        font_path="arial.ttf",
+        font_path=font,
         background_color="white",
         width=800,
         height=400,
